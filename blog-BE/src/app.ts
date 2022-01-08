1c05126ca8020a3e75f01c42513e52260de564b5
Mohamed Khaled Abdelmeguid <mohamed.kh.abdelmeguid@gmail.com>
2022-01-09 00:10:38 +0200

    chore: add mongoose and typegoose libraries. connect to mongo db

--- blog-BE/src/app.ts
+++ blog-BE/src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import './routes/index';
 import routes from './routes';
+import * as database from './database';
+import colors from 'colors';
+
 const app = express();
 const port = 3000;
-
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
-
 app.use(routes)
+
+database
+  .connectToMongo()
+  .then(() => console.info(colors.green('Up and Running!')))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
---
+++ blog-BE/src/database.ts
@@ -0,0 +1,11 @@
+import * as mongoose from 'mongoose'
+
+export async function connectToMongo():Promise<void> {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/test');
+    }
+    catch(err) {
+        console.log("did not connect to db");
+    }
+}
+  
